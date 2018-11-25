925c90b883123fbc59770bbd80ab40a48b6c0e67
ruianpan <ruianp2@illinois.edu>
2018-11-24 21:41:31 -0600

    ss

--- filter.java
+++ filter.java
@@ -5,7 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class filter {
-    public void main(String[] args){
+    public static void main(String[] args){
         try {
 
             InetAddress from = InetAddress.getByName(args[0]);
